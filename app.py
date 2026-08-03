d7dc0651d8efec584cbaf67c80f604c6f9db20a7
suseto-ui <suseto.servis@gmail.com>
2026-08-03 20:18:15 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 from services.config import get_config
 from routes.core_routes import core_bp
 
-app = Flask(__name__, template_folder="templates", static_folder="static")
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config.from_object(get_config())
 app.secret_key = app.config['SECRET_KEY']
 app.register_blueprint(core_bp)
 
-if __name__ == "__main__":
-    app.run(debug=app.config.get("DEBUG", False))
+if __name__ == '__main__':
+    app.run(debug=app.config.get('DEBUG', False))
