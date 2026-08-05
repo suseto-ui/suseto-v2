382986fe25ff866167cb26d3882eecb3a7e5f6c7
suseto-ui <suseto.servis@gmail.com>
2026-08-05 19:50:39 +0200

    Refactor: centralize route helpers (current_user, require_role, body)

--- routes/helpers.py
+++ routes/helpers.py
@@ -1,7 +1,16 @@
-from flask import request
+from flask import request, session
+
 
 def current_user():
-    return 'anonymous'
+    if session.get("username"):
+        return {"username": session.get("username"), "role": session.get("role")}
+    return None
+
+
+def require_role(*roles):
+    user = current_user()
+    return bool(user and user.get("role") in roles)
+
 
 def body():
     return request.get_json(silent=True) or {}
